3425a6056a31b57fbeee2662b4f6272b3dc3610f
Alexander Hagen <hagale13@student.westerdals.no>
2015-02-26 11:20:48 +0100

    gjort masse endringer med animasjoner...

--- sprint4/Kode/Foundation/Barn/js/showDivsScript.js
+++ sprint4/Kode/Foundation/Barn/js/showDivsScript.js
@@ -1,20 +1,22 @@
 $(function(){
 
-	var $barnehageButton, $kanalSButton;
+	var $barnehageButton, $kanalSButton, $skoleveienButton;
 
 	var $barnehageDivs, $kanalSDivs;
 
 	function setHTMLObjects(){
 		$barnehageButton = $("#barnehageButton");
+		$skoleveienButton =$("#skoleveienButton");
 		$kanalSButton = $("#kanalSButton");
 		$barnehageDivs = $(".barnehage");
+		$skoleveienMenuDivs = $("#skoleveienMenu");
 		$kanalSDivs = $(".kanalS");
 		
 	}
 
 	function buttonEvents(){
 		$barnehageButton.click(function(){
-			if($barnehageDivs.is(":hidden")){
+			if($barnehageDivs.is(":hidden") && $skoleveienMenuDivs.is(":hidden")){
 			$barnehageDivs.slideDown();
 				if($kanalSDivs.is(":visible")){
 					($kanalSDivs).slideUp();
@@ -24,8 +26,23 @@ $(function(){
 			}
 		});
 
+		$skoleveienButton.click(function(){
+			if($skoleveienMenuDivs.is(":hidden")){
+			$skoleveienMenuDivs.slideDown();
+				if($barnehageDivs.is(":visible")){
+					($barnehageDivs).slideUp();
+				}
+				if($kanalSDivs.is(":visible")){
+					($kanalSDivs).slideUp();
+				}	
+			}else {
+			$skoleveienMenuDivs.slideUp();
+			}
+		});
+	
+
 		$kanalSButton.click(function(){
-			if($kanalSDivs.is(":hidden")){
+			if($kanalSDivs.is(":hidden") && $skoleveienMenuDivs.is(":hidden")){
 			$kanalSDivs.slideDown();
 				if($barnehageDivs.is(":visible")){
 					($barnehageDivs).slideUp();
