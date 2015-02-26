ead55f5811ad168b8deb0c2d5efe8ba2ceb97486
tmbv93 <tmbv93@gmail.com>
2015-02-26 11:30:15 +0100

    lagt til metoder for å opprette sessions, og bekreftelse på at man logger seg inn

--- sprint4/salabytest/app/helpers/sessions_helper.rb
+++ sprint4/salabytest/app/helpers/sessions_helper.rb
@@ -1,8 +1,18 @@
 module SessionsHelper
 
-  # behandler logg_inn
-
+  # Behandler logg_inn
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  # Returnerer nåværende brukersession
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # Returnerer true hvis logged inn
+  def logged_in?
+    !current_user.nil?
+  end
+
 end
