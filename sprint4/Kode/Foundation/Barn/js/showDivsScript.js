bdd039ec3b982ce0c1d0fe381de1b6abfae10fb2
Alexander Hagen <hagale13@student.westerdals.no>
2015-02-26 11:55:10 +0100

    jobbet med anaimasjoner

--- sprint4/Kode/Foundation/Barn/js/showDivsScript.js
+++ sprint4/Kode/Foundation/Barn/js/showDivsScript.js
@@ -1,31 +1,37 @@
 $(function(){
 
-	var $barnehageButton, $kanalSButton, $skoleveienButton;
+	var $barnehageButton, $kanalSButton, $skoleveienButton,$MFLButton;
 
-	var $barnehageDivs, $kanalSDivs;
+	var $barnehageDivs, $kanalSDivs, $skoleveienMenuDivs, $MFLDivs;
 
 	function setHTMLObjects(){
 		$barnehageButton = $("#barnehageButton");
 		$skoleveienButton =$("#skoleveienButton");
 		$kanalSButton = $("#kanalSButton");
+		$MFLButton = $("#MFLButton");
+
 		$barnehageDivs = $(".barnehage");
 		$skoleveienMenuDivs = $("#skoleveienMenu");
 		$kanalSDivs = $(".kanalS");
+		$MFLDivs = $(".MFL");
 		
 	}
 
 	function buttonEvents(){
+		// For klikk på barnehage knapp
 		$barnehageButton.click(function(){
 			if($barnehageDivs.is(":hidden") && $skoleveienMenuDivs.is(":hidden")){
 			$barnehageDivs.slideDown();
 				if($kanalSDivs.is(":visible")){
 					($kanalSDivs).slideUp();
 				}
+
 			}else{
 			$barnehageDivs.slideUp();
 			}
 		});
 
+		// For klikk på skoleveien knapp
 		$skoleveienButton.click(function(){
 			if($skoleveienMenuDivs.is(":hidden")){
 			$skoleveienMenuDivs.slideDown();
@@ -40,7 +46,7 @@ $(function(){
 			}
 		});
 	
-
+		// For klikk på kanalS knapp
 		$kanalSButton.click(function(){
 			if($kanalSDivs.is(":hidden") && $skoleveienMenuDivs.is(":hidden")){
 			$kanalSDivs.slideDown();
@@ -49,6 +55,24 @@ $(function(){
 				}
 			}else{
 			$kanalSDivs.slideUp();
+			}
+		});
+
+		// For klikk på MFL knapp
+		$MFLButton.click(function(){
+			if($MFLDivs.is(":hidden")){
+			$MFLDivs.slideDown();
+				if($barnehageDivs.is(":visible")){
+					($barnehageDivs).slideUp();
+				}
+				if($skoleveienMenuDivs.is(":visible")){
+					($skoleveienMenuDivs).slideUp();
+				}
+				if($kanalSDivs.is(":visible")){
+					($kanalSDivs).slideUp();
+				}
+			}else{
+			$MFLDivs.slideUp();
 			}
 		});
 	}
