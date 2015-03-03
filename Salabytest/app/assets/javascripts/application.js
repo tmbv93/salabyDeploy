268c2637b355ae0b5f07014b195ec4501ce4db37
tmbv93 <tmbv93@gmail.com>
2015-03-03 15:17:58 +0100

    alle stylesheets er nå med

--- Salabytest/app/assets/javascripts/application.js
+++ Salabytest/app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.cookie
-//= require foundation
+//= require foun
 //= require turbolinks
 //= require fastclick
 //= require placeholder
