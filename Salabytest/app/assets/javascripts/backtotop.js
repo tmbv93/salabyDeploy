cf53594d8233c65b2a3102d73b2f60de20b98079
Alexander Hagen <hagale13@student.westerdals.no>
2015-04-28 15:15:51 +0200

    backToTop og hovedmeny barn
    fiska på back to top pila, og jobbet med skriptet til hovedmenyen til
    forsiden for barn

--- Salabytest/app/assets/javascripts/backtotop.js
+++ Salabytest/app/assets/javascripts/backtotop.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($){
 	// browser window scroll (in pixels) after which the "back to top" link is shown
-	var offset = 100,
+	var offset = 200,
 		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
 		offset_opacity = 1200,
 		//duration of the top scrolling animation (in ms)
