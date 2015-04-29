2f757456124fea02accaadd36ab8704681e2126d
tmbv93 <tmbv93@gmail.com>
2015-04-29 12:04:13 +0200

    Headere vises nå på rett sted

--- Salabytest/app/controllers/static_pages/info_controller.rb
+++ Salabytest/app/controllers/static_pages/info_controller.rb
@@ -1,25 +1,33 @@
 class StaticPages::InfoController < ApplicationController
   def bestilling
+    @include_header_info = true
   end
 
   def informasjon
+    @include_header_info = true
   end
 
   def kompetanse
+    @include_header_info = true
   end
 
   def omByparken
+    @include_header_info = true
   end
 
   def omKanalS
+    @include_header_info = true
   end
 
   def omSalaby
+    @include_header_info = true
   end
 
   def omSkoleveien
+    @include_header_info = true
   end
 
   def forside
+    @include_header_info = true
   end
 end
--- Salabytest/app/controllers/static_pages_controller.rb
+++ Salabytest/app/controllers/static_pages_controller.rb
@@ -11,7 +11,6 @@ class StaticPagesController < ApplicationController
   end
 
   def forside
-    @include_header_info = true
   end
 
   def admin
